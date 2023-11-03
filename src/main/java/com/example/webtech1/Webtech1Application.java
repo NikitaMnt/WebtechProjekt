ddf390a4e2ad0c8ae93f5c1c78902f8cab75c35a
NikitaMnt <nikita.mantel@student.htw-berlin.de>
2023-11-03 18:10:39 +0100

    1. Versuch

--- src/main/java/com/example/webtech1/Webtech1Application.java
+++ src/main/java/com/example/webtech1/Webtech1Application.java
@@ -3,6 +3,7 @@ package com.example.webtech1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Webtech1Application {
 
